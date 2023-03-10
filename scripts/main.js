7249a649418a8d3f5ddcc254716f83c15f73e839
LoickDev97438 <loickcherimont@gmail.com>
2023-03-10 22:34:42 +0100

    Allow article deletion and update current cart

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // MAIN
-const cart = [];
+let cart = [];
 const products = getFruits();
 
 displayUi(products);
@@ -7,7 +7,22 @@ saveCart(cart);
 
 // FUNCTIONS
 function saveCart(cart) {
-    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem("cart", JSON.stringify(cart)); // localstorage save
+}
+
+function updateUiCart(cart) {
+    // reset of cart
+    const uiCart = document.getElementById("cart");
+    uiCart.innerHTML = null;
+
+    cart.forEach(product => {
+        let {name, quantity} = product;
+
+        let li = document.createElement("li");
+        li.innerText = `${name.toUpperCase()} x ${quantity}`;
+
+        uiCart.appendChild(li);
+    })
 }
 
 // Temp Function : All available products
@@ -48,6 +63,7 @@ function displayUi(products) {
             <p class="price card-text">${price.toFixed(2)}€</p>
             <p class="card-text">No. <span class="imei fw-bold">${id}</span></p>
             <input type="button" class="add btn btn-primary" value="Add">
+            <input type="button" class="delete btn btn-danger" value="Remove">
         </div>`;
         // --
         card
@@ -55,6 +71,9 @@ function displayUi(products) {
             .addEventListener("click", addToCart);
 
         // Todo : Add a remove button
+        card
+            .querySelector(".delete")
+            .addEventListener("click", removeFromCart);
 
         document.getElementById("products").appendChild(card);
     }
@@ -73,5 +92,29 @@ function addToCart(e) {
     }
 
     saveCart(cart);
-    console.log(cart);
+    updateUiCart(cart);
+}
+
+function removeFromCart(e) {
+
+    let productImei = e.currentTarget.parentNode.querySelector(".imei").innerText;
+
+    const foundProduct = cart.find(product => product.id === productImei);
+
+    
+
+    if(foundProduct) {
+        if(foundProduct.quantity - 1 === 0) {
+            cart = cart.filter(product => product.id !== productImei);
+        } else {
+            foundProduct.quantity--;
+        } 
+    }
+
+    else {
+        return false;
+    }
+
+    saveCart(cart);
+    updateUiCart(cart);
 }
